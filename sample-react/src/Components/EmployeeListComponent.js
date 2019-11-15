ca06ceee029ffc72d02cc81f2113fcbd5830bdb6
Kumar Ramalingam <kramali1@visteon.com>
2019-11-15 10:14:44 +0530

    Array of object loops using map

---
+++ sample-react/src/Components/EmployeeListComponent.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+let employeesObject = [{
+        name: "Mohamd",
+        gender: "Male",
+        age: 20,
+        id:1
+    },
+    {
+        name: "Subhash",
+        gender: "Male",
+        age: 21,
+        id:2
+    },
+    {
+        name: "Kumar",
+        gender: "Male",
+        age: 25,
+        id:3
+    },
+    {
+        name: "Santhosh",
+        gender: "Male",
+        age: 28,
+        id:4
+    },
+    {
+        name: "Ravi",
+        gender: "Male",
+        age: 29,
+        id:5
+    }
+    ];
+
+function handleEmployeeList() {
+    return employeesObject.map((item, index) => {
+        return (
+            <li key={item.id}>
+               <div>{item.name}</div>
+               <div>{item.gender}</div>
+               <div>{item.age}</div>
+            </li>
+       )
+    });
+}
+
+function EmployeeListComponent(props) {
+  return(
+      <div>
+          EmployeeListComponent
+          <br />
+          <ul>
+          {handleEmployeeList()}
+          </ul>
+      </div>
+  );
+}
+
+export default EmployeeListComponent;
